37eaf87ae80efbadb91644b37ac679f8e0d52e29
thuattt <thuattt@payme.vn>
2023-05-24 04:31:13 +0700

    add

--- index.js
+++ index.js
@@ -12,19 +12,25 @@ import postRouter from "./routes/posts.js";
 import { studentRouter, userRouter } from "./routes/index.js";
 
 const app = express();
-const PORT = process.env?.PORT ?? 5000 ;
-// app.use() middware 
-app.use(checkToken) // guard
+const PORT = process.env?.PORT ?? 5000;
+// app.use() middware
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use('/user',postRouter)
-app.use('/user',userRouter);
-app.use('/students',studentRouter)
+app.get("/", async (req, res) => {
+  await res.status(200).json({
+    message: "connect success",
+  });
+});
+app.use(checkToken); // guard
+app.use("/user", postRouter);
+app.use("/user", userRouter);
+app.use("/students", studentRouter);
 
 const startServer = async () => {
   try {
-   await connectDB(process.env.MONGODB_URL);
+    await connectDB(process.env.MONGODB_URL);
     app.listen(PORT, () => {
       console.log("server has started on port http://localhost:8080");
     });
